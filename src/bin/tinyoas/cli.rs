dc58e44daf13c7469a584b40ef181ae97af8d4f0
eithery <eithery.pro@gmail.com>
2022-11-12 00:33:40 -0500

    Path arguments for build and inspect commands

--- src/bin/tinyoas/cli.rs
+++ src/bin/tinyoas/cli.rs
@@ -1,20 +1,24 @@
+use std::path::PathBuf;
 use clap::Command;
 use tinyoas::CliResult;
-use tinyoas::commands::{build, validate};
+use tinyoas::commands::{build, inspect};
 
 const BUILD: &str = "build";
-const VALIDATE: &str = "validate";
+const INSPECT: &str = "inspect";
 
 
 pub fn main() -> CliResult {
     let matches = cli().get_matches();
 
     match matches.subcommand() {
-        Some((BUILD, _)) => {
-            build()
+        Some((BUILD, options)) => {
+            let source = options.path_arg("source");
+            let target = options.path_arg("target");
+            build(source, target)
         }
-        Some((VALIDATE, _)) => {
-            validate()
+        Some((INSPECT, options)) => {
+            let source = options.path_arg("source");
+            inspect(source)
         }
         _ => unreachable!()
     }
@@ -27,15 +31,48 @@ fn cli() -> Command<'static> {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .version(clap::crate_version!())
-        .override_usage("tinyoas <COMMAND> <COMMAND_OPTIONS>")
+        .override_usage("tinyoas <COMMAND> <OPTIONS>")
 
         .subcommand(
-            Command::new(BUILD)
-                .about("Builds OAS 3 specification document from tinyOAS source")
+            clap::command!(BUILD)
+                .about("Build OpenAPI 3 specification documents from tinyOAS source")
                 .alias("b")
+                .args([
+                    source_path_arg(),
+                    target_path_arg()
+                ])
         )
         .subcommand(
-            Command::new(VALIDATE)
-                .about("Performs validation of tinyOAS source documents")
+            Command::new(INSPECT)
+                .alias("i")
+                .about("Inspect tinyOAS source documents")
+                .args([source_path_arg()])
         )
+}
+
+
+fn source_path_arg() -> clap::Arg<'static> {
+    clap::arg!(-s --source <PATH> "A path to tinyOAS source documents")
+        .required(false)
+        .default_value(".")
+        .value_parser(clap::value_parser!(PathBuf))
+}
+
+
+fn target_path_arg() -> clap::Arg<'static> {
+    clap::arg!(-t --target <PATH> "A path to built OpenAPI 3 specification documents")
+        .required(false)
+        .default_value(".")
+        .value_parser(clap::value_parser!(PathBuf))
+}
+
+
+trait PathArgs {
+    fn path_arg(&self, arg_name: &str) -> &PathBuf;
+}
+
+impl PathArgs for &clap::ArgMatches {
+    fn path_arg(&self, arg_name: &str) -> &PathBuf {
+        self.get_one::<PathBuf>(arg_name).unwrap()
+    }    
 }
--- src/tinyoas/commands.rs
+++ src/tinyoas/commands.rs
@@ -1,15 +1,19 @@
+use std::path::PathBuf;
 use crate::errors::CliError;
 
 pub type CliResult = Result<(), CliError>;
 
 
-pub fn build() -> CliResult {
-    println!("TODO: Build OAS3 spec is not implemented...");
+pub fn build(source: &PathBuf, target: &PathBuf) -> CliResult {
+    println!("Building OAS3 document...");
+    println!("Source: {:?}", source);
+    println!("Target: {:?}", target);
     Ok(())
 }
 
 
-pub fn validate() -> CliResult {
-    println!("TODO: tinyOAS validation is not implemented...");
+pub fn inspect(source: &PathBuf) -> CliResult {
+    println!("Inspect tinyOAS sources...");
+    println!("Source: {:?}", source);
     Ok(())
 }
